12fae495622b5cc29122bd456807caad01eaef38
fanyang <fanyang.coder@gmail.com>
2019-04-28 16:11:20 +0800

    add eos imp

--- eos/siderelay/src/chain.cpp
+++ eos/siderelay/src/chain.cpp
@@ -1,14 +1,14 @@
 #include <chain.hpp>
 
-void eosforce::send_transfer_core_token( const eosio::name& from,
+void chainspec::send_transfer_core_token( const eosio::name& from,
                                          const eosio::name& to,
                                          const eosio::asset& quantity,
                                          const std::string& memo ) {
    eosio::action{
       std::vector<eosio::permission_level>{{from, "active"_n}},
-      "eosio"_n,
+      "eosio.token"_n,
       "transfer"_n,
-      eosforce::transfer_args{
+      chainspec::transfer_args{
          from, to, quantity, memo
       }
    }.send();
--- eosforce/siderelay/ricardian/siderelay.contracts.md
+++
@@ -1,3 +0,0 @@
-<h1 class="contract"> out </h1>
-
-Stub for out action's ricardian contractrameters
